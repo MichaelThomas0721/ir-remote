f90ccb9e65ac2cc8f5ef1982ade3a69e069abf30
Michael Thomas <michaelthomas0721@icloud.com>
2024-01-28 15:35:24 -0500

    Add code to convert data to JSON and write to a file

--- remote.py
+++ remote.py
@@ -9,5 +9,15 @@ while True:
     data = decode(receive(8))
     if data:
         break
+
 keys['keyname'] = data
-print(json.dumps(prettify(keys), indent=2))
+
+# Convert the data to a pretty-printed JSON string
+json_data = json.dumps(prettify(keys), indent=2)
+
+# Print the data to the console
+print(json_data)
+
+# Write the data to a text file
+with open('decoded_ir_signal.txt', 'w') as file:
+    file.write(json_data)
